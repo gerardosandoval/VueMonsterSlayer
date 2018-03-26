1a9b00c1c259a4e82851f73c528becc10cb8b59b
Gerardo Sandoval <gsmgo.mail@gmail.com>
2018-03-26 00:26:00 -0600

    refactor a bit

--- app.js
+++ app.js
@@ -9,41 +9,69 @@ new Vue({
   methods: {
     giveUp: function() {
       answer = confirm("Press 'Ok' to be a coward")
-      if (answer == true ) { this.isPlaying = false; }
-      this.playerHealth = this.monsterHealth = 100
-      this.turnTrace = []
+      if (answer == true ) {
+        this.isPlaying = false
+        this.resetGame()
+      }
     },
     attack: function() {
-      var playerDemage = Math.floor((Math.random() * 10) + 1)
-      var monsterDemage = Math.floor((Math.random() * 10) + 1)
+      var playerDemage = this.demage(10, 1)
+      var monsterDemage = this.demage(10, 1)
       this.monsterHealth -= monsterDemage
-      this.turnTrace.unshift({from: "player", to: "monster", type: "attack", amount: monsterDemage})
-      vt = this
-      setTimeout( function() {
-        vt.playerHealth -= playerDemage
-        vt.turnTrace.unshift({from: "monster", to: "player", type: "attack", amount: playerDemage})
-      }, 250)
+      this.traceAtack("player", "monster", "attack", monsterDemage)
+      this.monsterAtack(playerDemage, "attack")
     },
     specialAttack: function() {
-      var playerDemage = Math.floor(Math.random() * (25 - 10 + 1)) + 10
-      var monsterDemage = Math.floor(Math.random() * (25 - 10 + 1)) + 10
+      var playerDemage = this.demage(25, 10)
+      var monsterDemage = this.demage(25, 10)
       this.monsterHealth -= monsterDemage
-      this.turnTrace.unshift({from: "player", to: "monster", type: "attack", amount: monsterDemage})
-      vt = this
-      setTimeout( function() {
-        vt.playerHealth -= playerDemage
-        vt.turnTrace.unshift({from: "monster", to: "player", type: "attack", amount: playerDemage})
-      }, 250)
+      this.traceAtack("player", "monster", "specialAttack", monsterDemage)
+      this.monsterAtack(playerDemage, "specialAttack")
     },
     heal: function() {
       var playerHeal = Math.floor((Math.random() * 15) + 1)
       var playerDemage = Math.floor((Math.random() * 10) + 1)
       this.playerHealth += playerHeal
-      this.turnTrace.unshift({from: "player", type: "heal", amount: playerHeal})
+      this.traceAtack("player", "", "heal", playerHeal)
+      this.monsterAtack(playerDemage, "attack")
+    },
+    checkScore: function() {
+      answer = null
+      if (this.monsterHealth < 1) {
+        answer = confirm("You win! new game?");
+      } else if (this.playerHealth < 1) {
+        answer = confirm("Monster wins! new game?");
+      }
+      if (answer == true ) {
+        this.resetGame()
+      } else if(answer == false) {
+        this.isPlaying = false
+      };
+    },
+    resetGame: function() {
+      this.playerHealth = this.monsterHealth = 100
+      this.turnTrace = []
+    },
+    demage: function(max, min) {
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    traceAtack: function(from, to, type, amount) {
+      this.turnTrace.unshift({from: from, to: to, type: type, amount: amount})
+    },
+    monsterAtack: function(playerDemage, type) {
+      vt = this
       setTimeout( function() {
         vt.playerHealth -= playerDemage
-        vt.turnTrace.unshift({from: "monster", to: "player", type: "attack", amount: playerDemage})
-      }, 250)
+        vt.traceAtack("monster", "player", type, playerDemage)
+        vt.checkScore()
+      }, 100)
+    }
+  },
+  watch: {
+    isPlaying: function() {
+      if (this.isPlaying == true){
+        this.resetGame()
+      }
     }
   }
 });
